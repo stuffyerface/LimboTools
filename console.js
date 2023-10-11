73ddd9ac85171b84406a156f18991339908e37ae
stuffyerface <77872467+stuffyerface@users.noreply.github.com>
2023-10-10 23:37:08 -0400

    Create console.js

---
+++ console.js
@@ -0,0 +1,11 @@
+register("command", () => {
+	const ItemStack = Java.type('net.minecraft.item.ItemStack');
+	const Enchantment = Java.type('net.minecraft.enchantment.Enchantment');
+	let items = ["minecraft:diamond_axe", "minecraft:diamond_pickaxe", "minecraft:shears"]
+	for(x in items){
+		let nbt = new ItemStack(new Item(items[x]).getItem())
+		nbt.func_77966_a(Enchantment.field_77349_p ,10)
+		Player.getInventory().getInventory().func_70441_a(nbt)
+	}
+	ChatLib.chat("&aSuccessfully gave Toolkit")
+}).setName("tools", true);
